eaca057f5ca9f15740664cf85069c5e1a5a51673
Hu Kaixiang <maxhu787@gmail.com>
2022-12-18 12:23:34 +0800

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  // console.log('user connected');
+  socket.on('user-connect', (username) => {
+    console.log(`user ${username} connected`);
+    io.emit('user-connect', username);
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+    
+  socket.on('message-submit', (msg) => {
+    io.emit('message-submit', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
